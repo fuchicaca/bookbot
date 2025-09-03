c47e62b9c7c41ac80838ba46d13134b23ce54e77
fuchicaca <adolfogutierrez1545@gmail.com>
2025-09-03 10:18:51 -0700

    C: bookbot now uses sys.arg to take in different filepaths

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import word_count
 from stats import character_count
 from stats import sort_char_counts
+import sys
 
 #takes filepath as input and returns the contents of the file as a string
 
@@ -15,18 +16,25 @@ def get_book_text(filepath):
 
 def main():
 
-	text = get_book_text("books/frankenstein.txt")
-	num_words = word_count(text)
-	ch_count = character_count(text)
-	descending = sort_char_counts(ch_count)
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	else:
+
+
+
+		text = get_book_text(sys.argv[1])
+		num_words = word_count(text)
+		ch_count = character_count(text)
+		descending = sort_char_counts(ch_count)
 	
-	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
-	print("----------- Word Count ----------")
-	print(f"Found {num_words} total words")
-	print("--------- Character Count -------")
-	for item in descending:
-		print(f"{item["char"]}: {item["num"]}")
-	print("============= END ===============")
+		print("============ BOOKBOT ============")
+		print(f"Analyzing book found at {sys.argv[1]}...")
+		print("----------- Word Count ----------")
+		print(f"Found {num_words} total words")
+		print("--------- Character Count -------")
+		for item in descending:
+			print(f"{item["char"]}: {item["num"]}")
+		print("============= END ===============")
 if __name__ == "__main__":
 	main()
